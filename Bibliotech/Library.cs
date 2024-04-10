eb13d11bd88330a2ba59ddf3ad63930801aba01d
Zam <samuel.kodehode@gmail.com>
2024-04-10 22:54:00 +0200

    init

---
+++ Bibliotech/Library.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Reflection;
+using System.Text.Json;
+
+namespace Bibliotech;
+
+public class Library
+{
+    private List<Book>? books = [];
+
+    public void AddBook()
+    {
+        Console.Write("Book title: ");
+        var title = Console.ReadLine()!;
+
+        Console.Write("Book author: ");
+        var author = Console.ReadLine()!;
+
+        Console.Write("Book year: ");
+        var year = int.Parse(Console.ReadLine()!);
+
+        var book = new Book(title, author, year);
+
+        books.Add(book);
+    }
+
+    public void RemoveBook()
+    {
+        var title = Console.ReadLine();
+        var bookToRemove = books.Find(book => book.Title == title);
+
+        if (bookToRemove != null)
+        {
+            books.Remove(bookToRemove);
+        }
+        else
+        {
+            Console.WriteLine($"{title} not found");
+        }
+    }
+
+    public void ViewAllBooks()
+    {
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
+
+    public void SaveLibrary()
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        var json = JsonSerializer.Serialize(books, options: options);
+        File.WriteAllText("library.json", json);
+    }
+
+    public void LoadLibrary()
+    {
+        if (File.Exists("library.json"))
+        {
+            var json = File.ReadAllText("library.json");
+            books = JsonSerializer.Deserialize<List<Book>>(json);
+        }
+    }
+}
---
+++ Bibliotech/Program.cs
@@ -0,0 +1,36 @@
+﻿using Bibliotech;
+
+var library = new Library();
+var isRunning = true;
+
+library.LoadLibrary();
+
+while (isRunning)
+{
+    Console.WriteLine("Welcome to the Bibliotech");
+    Console.WriteLine("1. add book");
+    Console.WriteLine("2. remove book");
+    Console.WriteLine("3. view books");
+    Console.WriteLine("4. exit");
+
+    var input = Console.ReadLine()!;
+
+    switch (input)
+    {
+        case "1":
+            library.AddBook();
+            library.SaveLibrary();
+            break;
+        case "2":
+            library.RemoveBook();
+            library.SaveLibrary();
+            break;
+        case "3":
+            library.ViewAllBooks();
+            break;
+        default:
+            library.SaveLibrary();
+            isRunning = false;
+            break;
+    }
+} 
